005bb24aa93567fa65dcd154a6c2fe326f48046e
zedz65 <zedz65@hotmail.com>
2020-04-13 20:38:27 +0000

    check

--- application1/application/routes.py
+++ application1/application/routes.py
@@ -2,11 +2,11 @@ from flask import render_template
 from application import app
 import requests
 
-@app.route('/' , methods=['GET'])
+@app.route('/', methods=['GET'])
 def home(): 		
     return render_template ('home.html', title = 'Home')
 
-@app.route('/get / generator' , methods=['GET', 'POST'])    
+@app.route('/get/generator', methods=['GET', 'POST'])    
 def accountref():
 
     three_lower = requests.get('http://application2:5001/generator/three_lower')	 
@@ -14,12 +14,12 @@ def accountref():
  
     return render_template ('home.html', title = 'Prize Generator', three_lower=three_lower.text get_sixdig=get_sixdig.text 
 
-# post is to do with service 4 as it posts the generated letters & numbers - not sure yet how to do this but think it is:
-#@app.route (??)
-# def prize ():
-#
-#  three_lower = requests.post(‘http://application2:5003/generator/three_lower’, data= three_lower.text) it is posting back to container for service 4? #
-# get_sixdig = requests.post(‘http://application3:5003/generator/get_sixdig’, data=get_sixdig.text)#
+#post is to do with service 4 as it posts the generated letters & numbers - not sure yet how to do this but think it is:
+@app.route (??)
+def prize ():
+
+    three_lower = requests.post(‘http://application2:5003/generator/three_lower’, data= three_lower.text) it is posting back to container for service 4? #
+    get_sixdig = requests.post(‘http://application3:5003/generator/get_sixdig’, data=get_sixdig.text)#
 
 
 
