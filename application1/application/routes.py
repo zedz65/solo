01834bcc366e444a3fbd4d240ccf4707a60ac9c1
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-04-15 22:32:28 +0100

    Update routes.py

--- application1/application/routes.py
+++ application1/application/routes.py
@@ -1,5 +1,6 @@
 from application import app
 from flask import render_template, request, Response
+import requests
 
 
 @app.route('/', methods=['GET'])
@@ -10,9 +11,9 @@ def home():
 def accountref():
 
     three_lower = requests.get('http://application2:5001/generator/three_lower')	 
-    get_sixdig = requests.get('http://application3:5002/generator/get_sixdig')
+    get_sixdig = requests.get('http://application3:5002/generator/sixdig')
  
-    return render_template('prize.html', title='prize', three_lower=three_lower.text, get_sixdig=get_sixdig.text) 
+    return render_template('home.html', title='home', three_lower=three_lower.text, get_sixdig=get_sixdig.text) 
 
 #post is to do with service 4 as it posts the generated letters & numbers - not sure yet how to do this but think it is:
 #@app.route (??)
