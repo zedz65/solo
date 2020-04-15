57b0b1f5440a947520d35ec572bfb90158edffa6
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-04-15 22:47:52 +0100

    Update routes.py

--- application1/application/routes.py
+++ application1/application/routes.py
@@ -3,11 +3,7 @@ from flask import render_template, request, Response
 import requests
 
 
-@app.route('/', methods=['GET'])
-def home(): 		
-    return render_template('home.html', title='Home')
-
-@app.route('/get/generator', methods=['GET'])    
+@app.route('/', methods=['GET'])   
 def accountref():
 
     three_lower = requests.get('http://application2:5001/generator/three_lower')	 
