7ca3557fb9a30fdf367e593c6e7971a38875b137
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-04-15 23:21:09 +0100

    Update routes.py

--- application1/application/routes.py
+++ application1/application/routes.py
@@ -12,11 +12,13 @@ def accountref():
     return render_template('home.html', title='home', three_lower=three_lower.text, get_sixdig=get_sixdig.text) 
 
 #post is to do with service 4 as it posts the generated letters & numbers - not sure yet how to do this but think it is:
-#@app.route (??)
-#def prize ():
+@app.route('/prize', methods=['GET', 'POST'])
+def prize():
 
    # three_lower = requests.post(‘http://application2:5003/generator/three_lower’, data= three_lower.text) it is posting back to container for service 4? #
    # get_sixdig = requests.post(‘http://application3:5003/generator/get_sixdig’, data=get_sixdig.text)#
+
+    return render_template('prize.html', title='Prize')
 
 
 
